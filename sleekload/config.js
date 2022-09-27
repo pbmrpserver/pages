9c798417372a9158275c4cbf055a1f40d838528b
pBMRP Server Account <112585294+pbmrpserver@users.noreply.github.com>
2022-09-27 15:50:55 -0400

    Add files via upload

---
+++ sleekload/config.js
@@ -0,0 +1,176 @@
+//  ____  _           _    _                    _
+// / ___|| | ___  ___| | _| |    ___   __ _  __| |
+// \___ \| |/ _ \/ _ \ |/ / |   / _ \ / _` |/ _` |
+//  ___) | |  __/  __/   <| |__| (_) | (_| | (_| |
+// |____/|_|\___|\___|_|\_\_____\___/ \__,_|\__,_|
+// version 2.18.0
+
+// Loading screen language
+//
+// You can create or edit languages by
+// editing/creating files in the languages folder
+//
+// Default available languages:
+// "brazilian_portuguese"
+// "english"
+// "polish"
+// "russian"
+config.language = "english"
+
+// Use an image for the logo instead of text
+config.logo_use_image = true
+
+// Image filename for the server logo
+// (Place the image file in the images folder)
+config.logo_image = "logo.png"
+
+// Text to be shown on logo
+config.logo_text = "pBMRP"
+
+// Use the server's name for the logo
+config.logo_servername = false
+
+// Center logo?
+// "true" or "false"
+config.logo_center = false
+
+// Display current map and gamemode?
+// "true" or "false"
+config.logo_show_info = true
+
+// Use a video for the background?
+// Otherwise it will use images
+// "true" or "false"
+config.background_use_video = false
+
+// Default available videos:
+// "blue_rays.webm"
+// "purple_blue_rays.webm"
+// "green_rays.webm"
+//
+// You can add your own video backgrounds
+// Just make sure they are using webm VP8 format
+// (Place the video file in the backgrounds/videos folder)
+config.background_video = "purple_blue_rays.webm"
+
+// (Image-background only)
+// Enable map-based background image?
+// If enabled, the background will be an image based on the server current map
+// You need to have a background image with the same name as the map
+// Also, the image needs to be in .jpg format
+// E.g: gm_flatgrass.jpg, gm_construct.jpg, rp_downtown_v4c_v2.jpg
+// "true" or "false"
+config.background_map_based = false
+
+// (Image-background only)
+// Place one or more images
+// Multiple images will automatically shuffle between them
+// (Place the image files in the backgrounds/images folder)
+config.background_images = [
+	"1.jpg",
+	"2.jpg",
+	"3.jpg",
+	"4.jpg",
+	"5.jpg",
+	"6.jpg",
+	"7.jpg",
+	"8.jpg",
+	"9.jpg",
+	"10.jpg",
+	"11.jpg",
+	"12.jpg",
+	"13.jpg",
+	"14.jpg",
+	"15.jpg",
+]
+
+// (Image-background only)
+// Random background images order?
+// "true" or "false"
+config.background_images_random_order = true
+
+// (Image-background only)
+// Delay between background images changes
+// in milliseconds
+config.background_images_duration = 5000
+
+// (Image-background only)
+// Image background fade duration
+// in milliseconds
+config.background_images_fade_duration = 2000
+
+// Enable background overlay?
+// "true" or "false"
+config.background_overlay = true
+
+// Background darkening amount
+// 0 for none (0%), 100 for pitch black (100%)
+config.background_darkening = 30
+
+// Play music during the loading screen?
+// "true" or "false"
+config.music_enable = false
+
+// Display current music name?
+// "true" or "false"
+config.music_show = false
+
+// Music playlist
+// Add as many youtube ids/urls or audio files you want
+// (YouTube only works on Chromium beta)
+// (Use ogg files for compatibility with default Gmod)
+// (Place the audio files in the music folder)
+config.music_playlist = [
+	{
+		name: "the Office Complex",
+		youtube: "https://www.youtube.com/watch?v=87Ew0HkqWfo",
+	},
+]
+
+// Random music order?
+// "true" or "false"
+config.music_random_order = true
+
+// Music volume
+// Choose a value between 0 (quietest) and 100 (loudest)
+config.music_volume = 1
+
+// Use Gmod volume for the music instead of manual volume
+config.music_gmod_volume = false
+
+// Enable custom messages?
+// "true" or "false"
+config.messages_enable = true
+
+// Enter your custom messages below
+config.messages_list = [
+	"We're almost there, Freeman. Get yourself in position.",
+	"Testing...testing... Everything seems to be in order.",
+	"Uh...it's probably not a problem...probably...but I'm showing a small discrepancy in...well, no, it's well within acceptable bounds again. Sustaining sequence.",
+	"We've boosted the Anti-Mass Spectrometer to 105 percent. Bit of a gamble, but we need the extra resolution.",
+	"I suspected this could happen but the Administrator just would not listen.",
+	"Do you suspect some alien subterfuge behind this failure?",
+	"Are you thinking what I'm thinking?",
+	"I've told you a hundred times, I am opposed to pushing this equipment beyond the test level.",
+	"It's much too unpredictable. Don't let it overcharge!",
+	"Nature made them. We were only studying them.",
+	"Put that thing down! Why must you security guards always insist on playing with our prototypes?",
+	"Do you think that, perhaps, I could have a crowbar of my own? Perhaps, then, an automatic rifle?",
+]
+
+// Random message order?
+// "true" or "false"
+config.messages_random_order = true
+
+// Delay between message changes
+// in milliseconds
+config.messages_delay = 10000
+
+// Messages fade duration
+// in milliseconds
+config.messages_fade_duration = 3000
+
+// Show errors inside Garry's Mod?
+// Can be useful for finding errors
+// "true" or "false"
+config.errors_show_ingame = true
